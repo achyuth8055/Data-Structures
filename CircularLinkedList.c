6335a20c1bb7459a5ac217fe4266a15636e33d28
Achyuth <120576551+achyuth8055@users.noreply.github.com>
2023-01-02 18:41:27 +0530

    Create CircularLinkedList.c

---
+++ CircularLinkedList.c
@@ -0,0 +1,92 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+
+struct Node{int data;struct Node *next;}*first;
+
+int length(struct Node *p)
+{
+   int len=0;
+    do
+    {
+    len++;
+    p=p->next;
+    }while(p != first);
+    return len;
+}
+void InsertCList(struct Node *p,int pos,int data)
+{
+            struct Node *temp;
+          if(pos < 0 || pos > length(p))
+            printf("Invalid Index \n");
+
+            if(pos == 0)
+            {
+                temp = (struct Node *)malloc(sizeof(struct Node));
+                temp->data = data;
+                if(first == NULL)
+                {
+                    first = temp;
+                    first->next = first;                
+                }
+                else {
+                        while(p->next != first) p = p->next;
+                        p->next = temp;
+                        temp->next = first;
+                        first = temp;
+                }               
+
+            }
+            else{
+                for(int i=0;i<pos-1;i++)
+                    p = p->next;
+                temp = (struct Node *)malloc(sizeof(struct Node));
+                temp->data = data;
+                temp->next = p->next;
+                p->next = temp;
+     
+            }
+        
+}
+
+void create(int data)
+{
+    struct Node *temp,*last;
+    if(first == NULL)
+    {   
+        first = (struct Node *)malloc(sizeof(struct Node));
+        first->data = data;
+        first->next = NULL;
+        last = first;
+    }
+    else{
+    
+    temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = NULL;
+    last->next = temp;
+    last = temp;
+    }
+    
+    last->next = first;
+}
+
+void display(struct Node *p)
+{       
+        do{
+            printf("%d \n",p->data);
+            p = p->next;
+        }
+        while(p != first);
+}
+
+int main() {
+    
+     create(1);
+     create(2);
+     create(3);
+     create(4);
+    InsertCList(first,0,5); //pointer ,index ,data
+    display(first);
+    return 0;
+}
